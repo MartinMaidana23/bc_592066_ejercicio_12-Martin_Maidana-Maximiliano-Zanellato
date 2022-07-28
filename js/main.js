0a44dc521d9efe19683e29c17eef4ac6b7bb0b0c
Maximiliano <euhkan@gmail.com>
2022-07-28 15:46:09 -0300

    bonus 99.9%?

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ console.log(titulo = document.querySelector("title").textContent)
 
 let btn1 = document.querySelector('#btn1')
 let btn2 = document.querySelector('#btn2')
-let btn3 = document.querySelector('#btn3')
 
 
 function capturarI1() {
@@ -21,68 +20,66 @@ function capturarI1() {
     apellido = document.querySelector('#apellido').value
     segundoApellido = document.querySelector('#segundoApellido').value
     integrante1 = new Integrante1(nombre, segundoNombre, apellido, segundoApellido)
-    console.log(integrante1);
     agregar1()
     stringI1()
     mostrarNombres()
-    comparacion()
+    comparacion()      
 }
 
 function capturarI2() {
     function Integrante2(nombre2, segundoNombre2, apellido2, segundoApellido2) {
-        this.nombre2=nombre2
-        this.segundoNombre2=segundoNombre2
-        this.apellido2=apellido2
-        this.segundoApellido2=segundoApellido2
+        this.nombre=nombre2
+        this.segundoNombre=segundoNombre2
+        this.apellido=apellido2
+        this.segundoApellido=segundoApellido2
     }
 
-    nombre2 = document.querySelector('#nombre2').value
-    segundoNombre2 = document.querySelector('#segundoNombre2').value
-    apellido2 = document.querySelector('#apellido2').value
-    segundoApellido2 = document.querySelector('#segundoApellido2').value
-
-    integrante2 = new Integrante2(nombre2, segundoNombre2, apellido2, segundoApellido2)
+    nombre = document.querySelector('#nombre2').value
+    segundoNombre = document.querySelector('#segundoNombre2').value
+    apellido = document.querySelector('#apellido2').value
+    segundoApellido = document.querySelector('#segundoApellido2').value
+    integrante2 = new Integrante2(nombre, segundoNombre, apellido, segundoApellido)
     agregar2()
     stringI2()
-    mostrarNombres()
+    mostrarNombres2()
     comparacion()
-    console.log(integrante2);
 }
 
-let integrantes = []
+let integrantes1 = []
+let integrantes2 = []
 
 function agregar1() {
-    integrantes.push(integrante1)
+    integrantes1.splice(0,0,integrante1)
 }
 
 function agregar2() {
-    integrantes.push(integrante2)
-}
-
-if (segundoNombre!=='') {
-    segundoNombre= ''+segundoNombre+''
-}else{
-    segundoNombre= ' '
+    integrantes2.splice(0,0,integrante2)
 }
 
 function stringI1() {
-    primerIntegrante=integrantes[0].nombre+" "+integrantes[0].segundoNombre+" "+integrantes[0].apellido+" "+integrantes[0].segundoApellido
+    primerIntegrante=`${integrantes1.nombre}+ +${integrantes1.segundoNombre}+ +${integrantes1.apellido}+ +${integrantes1.segundoApellido}`
 }
 
 function stringI2() {
-    segundoIntegrante=integrantes[1].nombre2+" "+integrantes[1].segundoNombre2+" "+integrantes[1].apellido2+" "+integrantes[1].segundoApellido2
+    segundoIntegrante=`${integrantes2.nombre}+ +${integrantes2.segundoNombre}+ +${integrantes2.apellido}+ +${integrantes2.segundoApellido}`
 }
 
 
 function mostrarNombres() {
-    console.log
     (`
         -----
         Integrante1: ${primerIntegrante}
+        -----
+    `);
+}
+function mostrarNombres2() {
+    (`
+        -----
         Integrante2: ${segundoIntegrante}
         -----
     `);
 }
+
 
 btn1.addEventListener('click', capturarI1)
 btn2.addEventListener('click', capturarI2)
@@ -90,45 +87,54 @@ btn2.addEventListener('click', capturarI2)
 
 
 function comparacion() {
-    datos = []
-    for (const {nombre: n, segundoNombre: sn, apellido: a, segundoApellido: sa} of integrantes) {
-        if (segundoNombre===''&&segundoApellido==='') {
-            datos.push(n,a,)
-        } else if (segundoApellido===''){
-            datos.push(n,sn,a)
-        }else if(segundoNombre==='') {
-            datos.push(n,a,sa)
-        }else {
-            datos.push(n,sn,a,sa)
-        }
+    let datos = []
+    let integrantes = []
 
-        console.log(datos);
+    
+    if (integrantes1.length > 1) {
+        integrantes1.pop()
     }
+    if (integrantes2.length > 1) {
+        integrantes2.pop()
+    }
+
+
+    if (integrantes1 != '') {
+        integrantes = integrantes1.concat(integrantes2)
+    } 
+    if (integrantes2 != '') {
+        integrantes = integrantes2.concat(integrantes1)
+    }
+
+    integrantes = integrantes.filter(element => {
+        return element != undefined
+    })
+
+    for (const {nombre: n, segundoNombre: sn, apellido: a, segundoApellido: sa} of integrantes) {
+        datos.push(n,sn,a,sa)
+    }
+    
+    datos = datos.filter(element => {
+        return element != undefined
+    })
+
     listaOrdenada = datos.sort()
     duplicados = []
-
   
     for (let i = 0; i < listaOrdenada.length; i++) {
         if (listaOrdenada[i + 1] === listaOrdenada[i]) {
-            if (listaOrdenada[i] != "") {
+            if (listaOrdenada[i] != '') {
             duplicados.push(listaOrdenada[i])
             } 
         }
     }
-    if (duplicados !== "") {
+    
+    if (duplicados != '') {
         console.log(`Hubo coincidencias`)
+/*         colorElegido = prompt("Ingrese un color para destacar los nombre: ") */
         console.log(duplicados)
     } else {
         console.log(`No hubo coincidencias`)
     }
-    
+
 }
-
-
-
-
-
-
-
-
-
