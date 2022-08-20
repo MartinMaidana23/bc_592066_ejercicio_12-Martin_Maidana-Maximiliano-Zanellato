ed5ed01790b588990c2a8364e7705b7b2861de46
Tincho <martinmaidana90@gmail.com>
2022-08-20 15:44:23 -0300

    listo

--- js/main.js
+++ js/main.js
@@ -3,9 +3,24 @@ console.log('Bootcamp 592066 - Martin Maidana - Maximiliano Zanellato')
 console.log(titulo = document.querySelector("title").textContent)
 
                                     
+//ARMADO ARRAYS CON LOS NOMBRES
 
-let int1 = document.querySelectorAll('.int1')
-let int2 = document.querySelectorAll('.int2')
+let int1 = Array.from(document.querySelectorAll('.int1'))
+let int2 = Array.from(document.querySelectorAll('.int2'))
+
+let nombres1 = [int1[0].innerText, int1[1].innerText];
+let apellidos1 = [int1[2].innerText.toUpperCase(), int1[3].innerText.toUpperCase()];
+let nombreCompleto1 = []
+nombreCompleto1 = nombreCompleto1.concat(nombres1, apellidos1).filter(x=>x.length>0)
+
+let nombres2 = [int2[0].innerText, int2[1].innerText];
+let apellidos2 = [int2[2].innerText.toUpperCase(), int2[3].innerText.toUpperCase()];
+let nombreCompleto2 = []
+nombreCompleto2 = nombreCompleto2.concat(nombres2, apellidos2).filter(x=>x.length>0)
+
+
+let duplicadosNombres=[]
+let duplicadosApellidos=[]
 
 int1.forEach(e=>{
     if (e.textContent!='') {
@@ -18,63 +33,78 @@ int2.forEach(e=>{
     }
 })
 
-let integranteA = Array.from(int1)
-let integranteB = Array.from(int2)
 
-primerIntegrante=[]
-segundoIntegrante=[]
-
-for(let i = 0; i< integranteA.length; i++){
-    primerIntegrante.push(integranteA[i].textContent)
-}
-for(let i = 0; i< integranteB.length; i++){
-    segundoIntegrante.push(integranteB[i].textContent)
-}
-
-integrante1 = [primerIntegrante[0], primerIntegrante[1],primerIntegrante[2].toUpperCase(), primerIntegrante[3].toUpperCase()]
-integrante2 = [segundoIntegrante[0], segundoIntegrante[1],segundoIntegrante[2].toUpperCase(), segundoIntegrante[3].toUpperCase()]
-
-i1= integrante1.filter(i=>i.length>0)
-i2= integrante2.filter(i=>i.length>0)
-
-
-
+// MOSTRAR EN CONSOLA LOS NOMBRES
 
 console.log(`
 --------
-Primer Integrante: "${i1.join(' ')}"
-Segundo Integrante: "${i2.join(' ')}"
+Primer Integrante: "${nombreCompleto1.join(' ')}"
+Segundo Integrante: "${nombreCompleto2.join(' ')}"
 --------
+
 `);
 
+//COMPARACION Y MOSTRAR DUPLICADO DE LOS NOMBRES
 
-// COMPARACION DE NOMBRES Y APELLIDOS //
-
-let duplicados=[]
-
-for (let i = 0; i < i1.length; i++ ){
-    for(let x = 0; x<i1.length; x++){
-        if (i1[i]==i2[x]) {
-            duplicados.push(i1[i])
+for (let i = 0; i < nombres1.length; i++ ){
+    for(let x = 0; x<nombres2.length; x++){
+        if (nombres1[i]===nombres2[x]) {
+            duplicadosNombres.push(nombres1[i])
         }
     }
 }
 
-// COINCIDENCIAS //
+if (duplicadosNombres != "") {
+    console.log(`Hubo coincidencias en los nombres: "${duplicadosNombres.join(', ')}"`)
+    alert(`Hubo coincidencias en los nombres: "${duplicadosNombres.join(', ')}"`)
+    colorElegido = prompt("Ingrese un color para destacar los nombre: ")
 
-if (duplicados != "") {
-    console.log(`Hubo coincidencias: "${duplicados.join(', ')}"`)
-    colorElegido = prompt("Ingrese un color para destacar los nombre: ") 
-} else {
-    console.log(`No hubo coincidencias`)
-}
-
-
-                        // CAMBIO DE COLOR A LOS DUPLICADOS //
-
-for (i = 0; i<duplicados.length;i++) {
-    const tomarElementos = document.getElementsByClassName(`${duplicados[i].toLowerCase()}`);
-    for (x = 0; x<tomarElementos.length;x++) {
-        tomarElementos[x].style.color = `${colorElegido}`
+    for (i = 0; i<duplicadosNombres.length;i++) {
+        const tomarElementos = document.getElementsByClassName(`${duplicadosNombres[i].toLowerCase()}`);
+        for (x = 0; x<tomarElementos.length;x++) {
+            tomarElementos[x].style.color = `${colorElegido}`
+        }
     }
+} else {
+    console.log(`No hubo coincidencias en los nombres`)
+    alert(`No hubo coincidencias en los nombres`)
 }
+
+
+
+// COMPARACION Y MOSTRAR DUPLICADO DE LOS APELLIDOS
+
+const compararApellidos = () =>{
+    let respuesta = prompt('¿Desea comparar los apellidos? \n 0: No || 1: Si ' )
+    if (respuesta === '1') {
+        for (let i = 0; i < apellidos1.length; i++ ){
+            for(let x = 0; x<apellidos2.length; x++){
+                if (apellidos1[i]===apellidos2[x]) {
+                    duplicadosApellidos.push(apellidos1[i])
+                }
+            }
+        }
+        
+        if (duplicadosApellidos != "") {
+            console.log(`Hubo coincidencias en los apellidos: "${duplicadosApellidos.join(', ')}"`)
+            alert(`Hubo coincidencias en los apellidos: "${duplicadosApellidos.join(', ')}"`)
+            colorElegido = prompt("Ingrese un color para destacar los nombre: ")
+
+            for (i = 0; i<duplicadosApellidos.length;i++) {
+                const tomarElementos = document.getElementsByClassName(`${duplicadosApellidos[i].toLowerCase()}`);
+                for (x = 0; x<tomarElementos.length;x++) {
+                    tomarElementos[x].style.color = `${colorElegido}`
+                }
+            }
+        } else {
+            console.log(`No hubo coincidencias en los apellidos`)
+            alert(`No hubo coincidencias en los apellidos`)
+        }
+    }
+    
+}
+
+compararApellidos()
+
+
+
