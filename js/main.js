e5df207ce7e5e1d722730595a6093b8cb80ff7c2
Tincho <martinmaidana90@gmail.com>
2022-08-20 14:08:01 -0300

    ejercicio arreglado

--- js/main.js
+++ js/main.js
@@ -1,140 +1,145 @@
-                                    /* PARTE 1 */
 console.log('Bootcamp 592066 - Martin Maidana - Maximiliano Zanellato')
 console.log(titulo = document.querySelector("title").textContent)
 
-                                    /* PARTE 2 */
-
+                                   
 let btn1 = document.querySelector('#btn1')
 let btn2 = document.querySelector('#btn2')
 
+let int1 = Array.from(document.querySelectorAll('.int1'))
+let int2 = Array.from(document.querySelectorAll('.int2'))
 
-function capturarI1() {
-    function Integrante1(nombre, segundoNombre, apellido, segundoApellido) {
-        this.nombre=nombre
-        this.segundoNombre=segundoNombre
-        this.apellido=apellido
-        this.segundoApellido=segundoApellido
-    }
-    nombre = document.querySelector('#nombre').value
-    segundoNombre = document.querySelector('#segundoNombre').value
-    apellido = document.querySelector('#apellido').value
-    segundoApellido = document.querySelector('#segundoApellido').value
-    integrante1 = new Integrante1(nombre, segundoNombre, apellido, segundoApellido)
-    agregar1()
-    stringI1()
-    mostrarNombres()
-    comparacion()      
-}
+let nombres1 = [];
+let apellidos1 = [];
+let nombreCompleto1 = []
 
-function capturarI2() {
-    function Integrante2(nombre2, segundoNombre2, apellido2, segundoApellido2) {
-        this.nombre=nombre2
-        this.segundoNombre=segundoNombre2
-        this.apellido=apellido2
-        this.segundoApellido=segundoApellido2
-    }
-
-    nombre = document.querySelector('#nombre2').value
-    segundoNombre = document.querySelector('#segundoNombre2').value
-    apellido = document.querySelector('#apellido2').value
-    segundoApellido = document.querySelector('#segundoApellido2').value
-    integrante2 = new Integrante2(nombre, segundoNombre, apellido, segundoApellido)
-    agregar2()
-    stringI2()
-    mostrarNombres2()
-    comparacion()
-}
-
-let integrantes1 = []
-let integrantes2 = []
-
-function agregar1() {
-    integrantes1.splice(0,0,integrante1)
-}
-
-function agregar2() {
-    integrantes2.splice(0,0,integrante2)
-}
-
-function stringI1() {
-    primerIntegrante=`${integrantes1.nombre}+ +${integrantes1.segundoNombre}+ +${integrantes1.apellido}+ +${integrantes1.segundoApellido}`
-}
-
-function stringI2() {
-    segundoIntegrante=`${integrantes2.nombre}+ +${integrantes2.segundoNombre}+ +${integrantes2.apellido}+ +${integrantes2.segundoApellido}`
-}
+let nombres2 = [];
+let apellidos2 = [];
+let nombreCompleto2 = []
 
 
-function mostrarNombres() {
-    (`
-        -----
-        Integrante1: ${primerIntegrante}
-        -----
-    `);
-}
-function mostrarNombres2() {
-    (`
-        -----
-        Integrante2: ${segundoIntegrante}
-        -----
-    `);
-}
+let duplicadosNombres=[]
+let duplicadosApellidos=[]
 
-
-btn1.addEventListener('click', capturarI1)
-btn2.addEventListener('click', capturarI2)
-
-
-
-function comparacion() {
-    let datos = []
-    let integrantes = []
-
-    
-    if (integrantes1.length > 1) {
-        integrantes1.pop()
-    }
-    if (integrantes2.length > 1) {
-        integrantes2.pop()
-    }
-
-
-    if (integrantes1 != '') {
-        integrantes = integrantes1.concat(integrantes2)
-    } 
-    if (integrantes2 != '') {
-        integrantes = integrantes2.concat(integrantes1)
-    }
-
-    integrantes = integrantes.filter(element => {
-        return element != undefined
+const leerForm1 = () =>{
+    int1.forEach(e=>{
+        if (e.value!='') {
+            e.className=e.value.toLowerCase()
+        }
     })
-
-    for (const {nombre: n, segundoNombre: sn, apellido: a, segundoApellido: sa} of integrantes) {
-        datos.push(n,sn,a,sa)
+    array1()
+    consola()
+    if (nombreCompleto2.length>0) {
+        compararNombres()
     }
-    
-    datos = datos.filter(element => {
-        return element != undefined
-    })
 
-    listaOrdenada = datos.sort()
-    duplicados = []
-  
-    for (let i = 0; i < listaOrdenada.length; i++) {
-        if (listaOrdenada[i + 1] === listaOrdenada[i]) {
-            if (listaOrdenada[i] != '') {
-            duplicados.push(listaOrdenada[i])
-            } 
+    if (nombreCompleto2.length>0) {
+        let respuesta = prompt('¿Desea comparar los apellidos? \n 0: No || 1: Si')
+        if (respuesta === '1') {
+            compararApellidos()
         }
     }
     
-    if (duplicados != '') {
-        console.log(`Hubo coincidencias`)
-/*         colorElegido = prompt("Ingrese un color para destacar los nombre: ") */
-        console.log(duplicados)
-    } else {
-        console.log(`No hubo coincidencias`)
+}
+
+const leerForm2 = () =>{
+    int2.forEach(e=>{
+        if (e.value!='') {
+            e.className=e.value.toLowerCase()
+        }
+    })
+    array2()
+    consola()
+    if (nombreCompleto1.length>0) {
+        compararNombres()
     }
 
+    if (nombreCompleto1.length>0) {
+        let respuesta = prompt('¿Desea comparar los apellidos? \n 0: No || 1: Si')
+        if (respuesta === '1') {
+            compararApellidos()
+        }
+    }
 }
+
+const array1 = () =>{
+
+    nombres1 = [int1[0].value, int1[1].value].filter(x=>x.length>0)
+    apellidos1 = [int1[2].value.toUpperCase(), int1[3].value.toUpperCase()].filter(x=>x.length>0)
+
+    nombreCompleto1 = nombreCompleto1.concat(nombres1, apellidos1)
+    
+    
+}
+
+const array2 = () =>{
+    nombres2 = [int2[0].value, int2[1].value].filter(x=>x.length>0)
+    apellidos2 = [int2[2].value.toUpperCase(), int2[3].value.toUpperCase()].filter(x=>x.length>0)
+
+    nombreCompleto2 = nombreCompleto2.concat(nombres2, apellidos2)
+}
+ 
+const consola = () =>{
+    if (nombreCompleto2[0]==="") {
+        console.log(`
+--------
+Primer Integrante: "${nombreCompleto1.join(' ')}"
+Segundo Integrante: ""
+--------
+        `);
+    }else if (nombreCompleto1[0]===""){
+        console.log(`
+--------
+Primer Integrante: ""
+Segundo Integrante: "${nombreCompleto2.join(' ')}"
+--------
+        `);
+    }else {
+        console.log(`
+--------
+Primer Integrante: "${nombreCompleto1.join(' ')}"
+Segundo Integrante: "${nombreCompleto2.join(' ')}"
+--------
+        `);
+    }
+}
+
+const compararNombres = () =>{
+    for (let i = 0; i < nombres1.length; i++ ){
+        for(let x = 0; x<nombres2.length; x++){
+            if (nombres1[i]===nombres2[x]) {
+                duplicadosNombres.push(nombres1[i])
+            }
+        }
+    }
+
+    if (duplicadosNombres != "") {
+        console.log(`Hubo coincidencias en los nombres: "${duplicadosNombres.join(', ')}"`)
+        alert(`Hubo coincidencias en los nombres: "${duplicadosNombres.join(', ')}"`)
+    } else {
+        console.log(`No hubo coincidencias en los nombres`)
+        alert(`No hubo coincidencias en los nombres`)
+    }
+}
+
+const compararApellidos = () =>{
+    for (let i = 0; i < apellidos1.length; i++ ){
+        for(let x = 0; x<apellidos2.length; x++){
+            if (apellidos1[i]===apellidos2[x]) {
+                duplicadosApellidos.push(apellidos1[i])
+            }
+        }
+    }
+
+    if (duplicadosApellidos != "") {
+        console.log(`Hubo coincidencias en los apellidos: "${duplicadosApellidos.join(', ')}"`)
+        alert(`Hubo coincidencias en los apellidos: "${duplicadosApellidos.join(', ')}"`)
+    } else {
+        console.log(`No hubo coincidencias en los apellidos`)
+        alert(`No hubo coincidencias en los apellidos`)
+    }
+}
+
+
+
+btn1.addEventListener('click', leerForm1)
+btn2.addEventListener('click', leerForm2)
